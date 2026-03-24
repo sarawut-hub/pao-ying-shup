408967df7c3068e68c52b319cdd753fbe0870f37
sarawut-hub <sarawut.o@kubota.com>
2026-03-24 11:01:59 +0700

    feat: define player ID columns in Match model and update layout title

--- app/models/match.ts
+++ app/models/match.ts
@@ -1,3 +1,10 @@
+import { column } from '@adonisjs/lucid/orm'
 import { MatchSchema } from '#database/schema'
 
-export default class Match extends MatchSchema {}
+export default class Match extends MatchSchema {
+	@column({ columnName: 'player1_id' })
+	declare player1Id: number | null
+
+	@column({ columnName: 'player2_id' })
+	declare player2Id: number | null
+}
